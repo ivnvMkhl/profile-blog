c82478bd92c22a588ca94c1739f5b3a9f9cb0052
Михаил Иванов <mihailivanov@MacBook-Pro-Mihail.local>
2025-12-03 20:33:52 +0300

    add formatter

--- .eleventy.js
+++ .eleventy.js
@@ -1,49 +1,52 @@
 import { hashAssets, updateHashedLinks } from "build-hasher";
-import dotenv from 'dotenv';
-import { lectures, articles, projects, counterChart } from './objects.js';
+import dotenv from "dotenv";
+import { lectures, articles, projects, counterChart } from "./objects.js";
 
 dotenv.config();
 
-export default function(eleventyConfig) {
+export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy({
-    "src/static/.htaccess": ".htaccess"
+    "src/static/.htaccess": ".htaccess",
   });
 
-  eleventyConfig.addGlobalData("baseUrl", process.env.ELEVENTY_ENV === 'development' ? '' : process.env.BASE_URL);  
+  eleventyConfig.addGlobalData(
+    "baseUrl",
+    process.env.ELEVENTY_ENV === "development" ? "http://localhost:8080" : process.env.BASE_URL
+  );
 
   eleventyConfig.addGlobalData("lectures", lectures);
   eleventyConfig.addGlobalData("articles", articles);
   eleventyConfig.addGlobalData("projects", projects);
   eleventyConfig.addGlobalData("counterChart", counterChart);
 
-  eleventyConfig.on('afterBuild', async ({ runMode, outputMode, dir }) => {
-    console.log('🚀 Starting post-build processing...');
-    
+  eleventyConfig.on("afterBuild", async ({ runMode, outputMode, dir }) => {
+    console.log("🚀 Starting post-build processing...");
+
     try {
-      console.log('🚀 Starting file hashing...');
+      console.log("🚀 Starting file hashing...");
       const hashManifest = hashAssets({
         outputDir: dir.output,
-        extensions: ['.js', '.json', '.css'],
-        ignoredFiles: ['site.webmanifest.json']
+        extensions: [".js", ".json", ".css"],
+        ignoredFiles: ["site.webmanifest.json"],
       });
 
       updateHashedLinks({
         hashManifest: hashManifest,
         outputDir: dir.output,
-        extensions: ['.html', '.js'],
-        files: ['.htaccess'],
+        extensions: [".html", ".js"],
+        files: [".htaccess"],
         replaceRule: {
-          '.js': ['js/', './'],
+          ".js": ["js/", "./"],
         },
       });
-      console.log('✅ File hashing completed!');
-      console.log('✅ Post-build processing completed successfully!');
+      console.log("✅ File hashing completed!");
+      console.log("✅ Post-build processing completed successfully!");
     } catch (error) {
-      console.error('❌ Error during post-build processing:', error.message);
+      console.error("❌ Error during post-build processing:", error.message);
     }
   });
 
@@ -53,11 +56,11 @@ export default function(eleventyConfig) {
       output: "dist",
       includes: "_includes",
       layouts: "_layouts",
-      data: "_data"
+      data: "_data",
     },
     templateFormats: ["md", "njk", "html", "liquid"],
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk"
+    dataTemplateEngine: "njk",
   };
 }
--- objects.js
+++ objects.js
@@ -1,62 +1,62 @@
-
 export const lectures = {
-    "async-and-event-loop-in-javascript": {
-        name: "Асинхронность и EventLoop в JavaScript",
-        description: "Разбираем вопросы связанные с асинхронностью в JavaScript: стек вызовов функций, движок выполнения JS, что такое микротаски и макротаски, очереди и что попадает в microtask queue и macrotask queue, EventLoop и цикл работы браузера",
-        href: "/lectures/async-and-event-loop-in-javascript",
-        video: "https://vkvideo.ru/video_ext.php?oid=14453319&id=456239444&hash=061dcf0d7d0b8a0b&autoplay=1",
-        tags: [
-            {label: "JavaScript", color: "orange"},
-            {label: "TypeScript", color: "blue"},
-            {label: "Async", color: "red"}
-        ]
-    },
-    "oop-and-description-of-reality": {
-        name: "ООП и описание реальности",
-        description: "Разбираем подход ООП простыми словами с начинающими разработчиками",
-        href: "/lectures/oop-and-description-of-reality",
-        video: "https://vkvideo.ru/video_ext.php?oid=14453319&id=456239445&hash=10e9415a830fea40&autoplay=1",
-        tags: [
-            {label: "ООП", color: "violet"},
-            {label: "JavaScript", color: "orange"}
-        ]
-    },
-    "plus-and-minus-of-using-typescript": {
-        name: "Плюсы и минусы использования TypeScript",
-        description: "Разбираем плюсы и минусы использования TypeScript. Безопасность кода, строгая типизация, спорные моменты, документирование кода в TS. Type casting, enum, any и другие опасные моменты",
-        href: "/lectures/plus-and-minus-of-using-typescript",
-        video: "https://vkvideo.ru/video_ext.php?oid=14453319&id=456239446&hash=3933877fcaffab1a&autoplay=1",
-        tags: [
-            {label: "TypeScript", color: "blue"}
-        ]
-    },
+  "async-and-event-loop-in-javascript": {
+    name: "Асинхронность и EventLoop в JavaScript",
+    description:
+      "Разбираем вопросы связанные с асинхронностью в JavaScript: стек вызовов функций, движок выполнения JS, что такое микротаски и макротаски, очереди и что попадает в microtask queue и macrotask queue, EventLoop и цикл работы браузера",
+    href: "/lectures/async-and-event-loop-in-javascript",
+    video: "https://vkvideo.ru/video_ext.php?oid=14453319&id=456239444&hash=061dcf0d7d0b8a0b&autoplay=1",
+    tags: [
+      { label: "JavaScript", color: "orange" },
+      { label: "TypeScript", color: "blue" },
+      { label: "Async", color: "red" },
+    ],
+  },
+  "oop-and-description-of-reality": {
+    name: "ООП и описание реальности",
+    description: "Разбираем подход ООП простыми словами с начинающими разработчиками",
+    href: "/lectures/oop-and-description-of-reality",
+    video: "https://vkvideo.ru/video_ext.php?oid=14453319&id=456239445&hash=10e9415a830fea40&autoplay=1",
+    tags: [
+      { label: "ООП", color: "violet" },
+      { label: "JavaScript", color: "orange" },
+    ],
+  },
+  "plus-and-minus-of-using-typescript": {
+    name: "Плюсы и минусы использования TypeScript",
+    description:
+      "Разбираем плюсы и минусы использования TypeScript. Безопасность кода, строгая типизация, спорные моменты, документирование кода в TS. Type casting, enum, any и другие опасные моменты",
+    href: "/lectures/plus-and-minus-of-using-typescript",
+    video: "https://vkvideo.ru/video_ext.php?oid=14453319&id=456239446&hash=3933877fcaffab1a&autoplay=1",
+    tags: [{ label: "TypeScript", color: "blue" }],
+  },
 };
 
 export const articles = {
-    "how-i-got-tired-of-creating-files-and-folders": {
-        name: "Как мне надоело создавать файлы и папки", 
-        description: "Рассуждаю о проблеме регулярного создания файлов и о том, как поддержать архитектуру и автоматизировать рутину. Написал утилиту на Go для генерации структур файлов по шаблонам", 
-        href: "/articles/how-i-got-tired-of-creating-files-and-folders",
-        tags: [
-            {label: "Go", color: "red"},
-            {label: "Generate", color: "green"}
-        ]
-    },
+  "how-i-got-tired-of-creating-files-and-folders": {
+    name: "Как мне надоело создавать файлы и папки",
+    description:
+      "Рассуждаю о проблеме регулярного создания файлов и о том, как поддержать архитектуру и автоматизировать рутину. Написал утилиту на Go для генерации структур файлов по шаблонам",
+    href: "/articles/how-i-got-tired-of-creating-files-and-folders",
+    tags: [
+      { label: "Go", color: "red" },
+      { label: "Generate", color: "green" },
+    ],
+  },
 };
 
 export const projects = {};
 
 export const counterChart = {
-    "lectures": {
-        name: "Лекции",
-        value: Object.keys(lectures).length,
-    },
-    "articles": {
-        name: "Статьи",
-        value: Object.keys(articles).length,
-    },
-    "projects": {
-        name: "Проекты",
-        value: Object.keys(projects).length,
-    }
-}
+  lectures: {
+    name: "Лекции",
+    value: Object.keys(lectures).length,
+  },
+  articles: {
+    name: "Статьи",
+    value: Object.keys(articles).length,
+  },
+  projects: {
+    name: "Проекты",
+    value: Object.keys(projects).length,
+  },
+};
